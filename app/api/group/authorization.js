b5250666c3303e371c6fd4e6ffe54d37f713b649
Andrew Barba <abarba.77@gmail.com>
2015-11-19 22:23:42 -0500

    add forbidden error and group owner authorization

--- app/api/group/authorization.js
+++ app/api/group/authorization.js
@@ -1,9 +1,32 @@
 "use strict";
 
+const Errors = require('app/errors');
 const CommonAuth = require('api/common/authorization');
+
+// Modules
+const Group = require('modules/group');
 
 class GroupAuth extends CommonAuth {
 
+  /**
+   * Requires user to own group in url
+   *
+   * @method requiresGroupOwner
+   */
+  requiresGroupOwner(req, res, next) {
+    this.requiresLogin(req, res, err => {
+      if (err) return next(err);
+
+      Group
+        .read(req.params.id)
+        .pluck('userId')
+        .execute((err, group) => {
+          if (err || !group) return next(new Errors.NotFoundError());
+          if (group.userId !== req.userId) return next(new Errors.ForbiddenError());
+          next();
+        });
+    });
+  }
 }
 
 module.exports = new GroupAuth();
